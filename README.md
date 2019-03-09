4c35166c43955e0d15f6426673fa196d009fd31b
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2019-03-09 16:09:11 +0900

    Fix headers-msp430 install path

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ You may want to install `mspdebug-head` by the following commands.
 Version:
 
     binutils-msp430-2.22-20120911_2
-    headers-msp430-20130321_2
+    headers-msp430-20130321_3
     gcc-msp430-4.7.0-20120911_2
     libc-msp430-20120716_2
     gdb-msp430-7.2a-20111205_1
--- headers-msp430.rb
+++ headers-msp430.rb
@@ -3,14 +3,14 @@ class HeadersMsp430 < Formula
   homepage "https://sourceforge.net/projects/mspgcc/"
   url "https://downloads.sourceforge.net/project/mspgcc/msp430mcu/msp430mcu-20130321.tar.bz2"
   sha256 "01be411f8901362fab32e7e1be044a44e087a98f3bd2346ac167960cfe6fa221"
-  revision 2
+  revision 3
 
   def install
     target = "msp430"
 
     # Install headers and link scripts to target_include and target_lib.
-    target_include = "#{HOMEBREW_PREFIX}/include/#{target}/include"
-    target_lib = "#{HOMEBREW_PREFIX}/lib/#{target}/lib"
+    target_include = include/target/"include"
+    target_lib = lib/target/"lib"
     inreplace "scripts/install.sh" do |s|
       s.gsub! "INCPATH=${PREFIX}/#{target}/include", "INCPATH=#{target_include}"
       s.gsub! "LIBPATH=${PREFIX}/#{target}/lib", "LIBPATH=#{target_lib}"
